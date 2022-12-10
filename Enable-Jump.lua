400372e54577b7b0d20626a2ab0a2233eb6dced4
DylanIsAKingL <114356367+DylanIsAKingL@users.noreply.github.com>
2022-12-10 09:01:37 -0800

    Update Enable-Jump.lua

--- Enable-Jump.lua
+++ Enable-Jump.lua
@@ -5,7 +5,7 @@ if game.PlaceId == 5874128725 then
 	game.StarterGui:SetCore("SendNotification", {Title="Jump Enabled!"; Text="By DylanIsAKing#2402"; Duration=5;})
 	while true do
 		game.Players.LocalPlayer.Character.Humanoid.JumpPower = 50
-		wait(0.05)
+		wait(0.01)
 	end
 else
 	game.StarterGui:SetCore("SendNotification", {Title="This Game Is Unsupported!"; Text="By DylanIsAKing#2402"; Duration=5;})
