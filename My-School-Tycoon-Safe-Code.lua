327f429ec63d573599cbb85dbc3b57678f688724
DylanIsAKingL <114356367+DylanIsAKingL@users.noreply.github.com>
2022-12-18 13:04:30 -0800

    Create My-School-Tycoon-Safe-Code.lua

---
+++ My-School-Tycoon-Safe-Code.lua
@@ -0,0 +1,2 @@
+local str = game.Workspace.Badges.MastermindBadge.Codes
+game.StarterGui:SetCore("SendNotification", {Title=str.Code1.SurfaceGui.TextLabel.Text:gsub("%_", "")..str.Code2.SurfaceGui.TextLabel.Text:gsub("%_", "")..str.Code3.SurfaceGui.TextLabel.Text:gsub("%_", "")..str.Code4.SurfaceGui.TextLabel.Text:gsub("%_", ""); Text="By DylanIsAKing#2402"; Duration=15;})
