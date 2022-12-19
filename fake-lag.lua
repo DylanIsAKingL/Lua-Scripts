9aa8fb7bd812b8161940c604faa4052b197d4ab3
DylanIsAKingL <114356367+DylanIsAKingL@users.noreply.github.com>
2022-12-19 08:36:45 -0800

    Update fake-lag.lua

--- fake-lag.lua
+++ fake-lag.lua
@@ -1,6 +1,29 @@
-while true do
+local UIS = game:GetService("UserInputService")
+
+local toggled = true
+
+UIS.InputBegan:Connect(function(input)
+	if input.KeyCode == Enum.KeyCode.L and toggled then
+		toggled = false
+	elseif input.KeyCode == Enum.KeyCode.L and toggled == false then
+		toggled = true
+	end
+end)
+
+UIS.InputBegan:Connect(function(input, plr)
+	if input.KeyCode == Enum.KeyCode.K then
+		plr:Kick("Please check your internet connection and try again. (Error Code: 277)")
+	end
+end)
+
+while toggled do
 	task.wait(math.random(0.5,1.4))
 	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
 	task.wait(math.random(0.4,1.2))
 	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
+end
+
+while toggled == false and game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored == true do
+	game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = false
+	task.wait(0.1)
 end
