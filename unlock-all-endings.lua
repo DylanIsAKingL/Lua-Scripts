55790c957ec1abdf3400d17aeb4516f516bac7bb
DylanIsAKingL <114356367+DylanIsAKingL@users.noreply.github.com>
2022-12-22 09:56:21 -0800

    Create unlock-all-endings.lua

---
+++ unlock-all-endings.lua
@@ -0,0 +1,77 @@
+local args = {
+    [1] = "ShopliftEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "ChipsEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "OrangeEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "SleepEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "ToothpasteEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "UltraDeathEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "BreakfastEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "FireEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "FreezerEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "InsomniaEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "FloodedEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "EveryEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
+
+local args = {
+    [1] = "FinalEnding"
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.UpdateEndingValue:FireServer(unpack(args))
